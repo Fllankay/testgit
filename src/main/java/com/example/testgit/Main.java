1a7944aa147fdf4c640ef1694c59c3b21eda1ff8
Fllankay <625455283@qq.com>
2020-05-20 15:41:56 +0800

    创建本地分支

---
+++ src/main/java/com/example/testgit/Main.java
@@ -0,0 +1,13 @@
+package com.example.testgit;
+
+/**
+ * 
+ * @author Yuqb
+ * @date 2020年5月20日, 0020
+ * @version v1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is Yuqb");
+    }
+}
